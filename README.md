81aa86c6b5a15a9ae6a73bc9901f577440a22e89
Enxas <edvinasgud@gmail.com>
2018-05-15 11:15:54 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Requirements
+
+NodeJS v10
+
+NPM v6
+
+## Usage
+
+Go to directory `cd /4chan_image_downloader`
+
+Install dependancies `npm install`
+
+To download all images from thread https://boards.4chan.org/w/thread/1234567 execute command `node app.js --board w --thread 1234567`
+
+In this example /w/ is boards letter and '1234567' is thread id
---
+++ app.js
@@ -0,0 +1,94 @@
+var fs = require('fs');
+var request = require('request');
+var mkdirp = require('mkdirp');
+var program = require('commander');
+
+
+program
+  .description('An application for downloading all images from 4chan thread')
+  .option('-b, --board <n>', 'Board letter')
+  .option('-t, --thread <n>', 'Thread id', parseInt)
+  .parse(process.argv);
+  
+const board = program.board;
+const thread = program.thread;
+const dir = 'images/'+ thread;
+
+request({
+  url: 'https://a.4cdn.org/'+ board +'/thread/'+ thread +'.json',
+  json: true
+}, function(error, response, body) {
+	const imageList = [];
+	
+	if (body) {
+	for (let post in body.posts) {
+		if (body.posts[post].tim) {
+			imageList.push(body.posts[post].tim + body.posts[post].ext);
+		}
+	}
+	
+	// create folder structure for images
+	mkdirp(dir, function(err) { 
+		if (err) {
+			console.log(`Error while creating folder ${dir}. ${err}`);
+		}
+	});
+	
+	asyncDownload(board, imageList);
+} else {
+	console.log(`Couldn't find specified board "${board}" or thread id "${thread}"`);
+}
+
+});
+
+async function asyncDownload(board, imageList) {
+	console.log(`Found ${imageList.length} images, downloading to ${dir} !`);
+	for (let i = 0; i < imageList.length; i++) {
+		const imgPath = dir+'/'+ imageList[i];
+		if (fs.existsSync(imgPath)) {
+			console.log(`File ${imageList[i]} exists, skipping... (${i+1}/${imageList.length})`);
+			continue;
+		}
+		await download('https://i.4cdn.org/'+ board +'/'+ imageList[i], imgPath);
+		console.log(`Downloaded ${imageList[i]} (${i+1}/${imageList.length})`);
+	}
+	console.log(`Finished downloading all ${imageList.length} images!`);
+}
+
+function download(url, dest) {
+  return new Promise((resolve, reject) => {
+   
+    var file = fs.createWriteStream(dest);
+    var sendReq = request.get(url);
+
+    // verify response code
+    sendReq.on('response', function(response) {
+        if (response.statusCode !== 200) {
+			// return cb('Response status was ' + response.statusCode);
+			console.log('Response status was ' + response.statusCode);
+            reject();
+        }
+    });
+
+    // check for request errors
+    sendReq.on('error', function (err) {
+        fs.unlink(dest);
+		// return cb(err.message);
+		console.log(err.message);
+        reject();
+    });
+
+    sendReq.pipe(file);
+
+    file.on('finish', function() {
+        file.close(resolve());  // close() is async, call cb ( file.close(cb); ) after close completes.
+    });
+
+    file.on('error', function(err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        // return cb(err.message);
+		console.log(err.message);
+    });	
+   
+  });
+}
